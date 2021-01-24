56dc977e0f6612a3072478da28cf2b8d24682d04
Sveta <svetlanasdemianova5@gmail.com>
2021-01-24 23:41:37 +0300

    added tabs navigation in carousei.sass

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,24 @@ $(document).ready(function(){
         infinite: true,
         speed: 1200,
         slidesToShow: 1,
-        fade: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
+        slideToScroll: 1,
+        arrows:true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png"></button>',
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                  
+                  dots: true,
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+                }
+              },
+        ]
         
-      
-      
     });
   });
