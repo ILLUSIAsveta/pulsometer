be9a4ca4b7b1727b3eaf73bdf96badfc5c2b0fb4
Sveta <svetlanasdemianova5@gmail.com>
2021-01-24 18:23:56 +0300

    added blok carusel.sass

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        slidesToShow: 1,
+        fade: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
+        
+      
+      
+    });
+  });
